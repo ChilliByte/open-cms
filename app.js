5c8951169bb66bc61848c967b92d1cf659a7cd01
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-07-04 12:53:43 +0100

    More stuff

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var session = require('express-session');
+var connectMongoStore = require('connect-mongo')(session);
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', require('hbs').__express);
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  store:  new connectMongoStore({ url: app.get("mongo-dburl") })
+}))
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+module.exports = app;
