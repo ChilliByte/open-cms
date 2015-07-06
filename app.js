3a5b5a38a4c84e6f9f7143b1a5faf59936c0d83e
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-07-06 14:36:03 +0100

    Now introduced static hosting at public folder

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'keyboard cat',
   store:  new connectMongoStore({ url: process.env.DBURL })
