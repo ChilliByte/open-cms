e674a149860f1bb811fa164e873162d9d26b0ed7
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-07-06 15:13:20 +0100

    Logout and links added to Admin Panel

--- routes/admin.js
+++ routes/admin.js
@@ -11,6 +11,10 @@ function isAdmin(req,res,next){
 }
 
 router.get("/authorise", function(req,res){
+  var sess = req.session;
+  if(sess.isAdmin==true){
+    res.redirect("/admin/")
+  }
   var denied;
   if(req.query.denied=="true"){
     denied = true;
@@ -34,6 +38,20 @@ router.post("/authorise", function(req,res) {
       res.redirect("/admin/authorise?denied=true");
     }
   }
+});
+
+router.get("/admin/login", isAdmin, function(req,res){
+  res.redirect("/admin/authorise");
+});
+
+router.get("/admin/deauthorise", function(req, res){
+  var sess = req.session;
+  sess.isAdmin = false;
+  res.redirect("/");
+});
+
+router.get("/admin/logout", isAdmin, function(req,res){
+  res.redirect("/admin/deauthorise");
 });
 
 router.get("/", isAdmin, function(req,res){
