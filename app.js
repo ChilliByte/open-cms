a587d22f0a295f5cd887588174f4fe1b0f7fbea9
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-09-05 15:02:24 +0000

    Fixed things. Important for #1. Now more secure.

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/medium-editor', express.static(path.join(__dirname, 'node_modules/medium-editor/dist')));
 app.use(session({
   secret: 'keyboard cat',
   store:  new connectMongoStore({ url: process.env.DBURL })
