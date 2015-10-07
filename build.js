424cb123e49cff855cd434cf5b0f1bd44d30b82c
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2015-10-07 11:47:57 +0000

    Added an 'export-all' option

--- build.js
+++ build.js
@@ -31,4 +31,31 @@ router.get("/export-aliases.json", function(req,res){
   });
 });
 
+router.get("/export-all.json", function(req,res){
+  var pages = req.db.collection('pages');
+  var aliases = req.db.collection('aliases');
+  var collections = req.db.collection('collections');
+  pages.find().toArray(function(err1, arrPages) {
+    if(err1) console.log(err1);
+    else{
+      aliases.find().toArray(function(err2, arrAliases) {
+        if(err2) console.log(err2);
+        else{
+            collections.find().toArray(function(err3, arrCollections) {
+            if(err3) console.log(err3);
+            else{
+              res.json({
+                pages: arrPages,
+                aliases: arrAliases,
+                collections: arrCollections
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+
 module.exports = router;
